10a1c51bc71a53694c9e31b35a04fc46e329f858
Sepehr S. Yeganeh <sesesapp@gmail.com>
2026-08-20 01:36:00 +0330

    add file existance check

--- ecg_analyzer/io/ecg_loader.py
+++ ecg_analyzer/io/ecg_loader.py
@@ -4,17 +4,29 @@ from ecg_analyzer.models import ECGRecord, ECGLeads
 
 
 def load_ecg_file(
-        rec_path: Path,
+        rec_path: Path | str,
+        rec_name: Path | str,
         patient_id: str,
         metadata: dict = None
 ) -> ECGRecord:
     """
-    :param rec_path: path to ecg record file. Both file.dat and file.hea must be in same directory.
+    :param rec_path: path to ecg record file
+    :param rec_name: name of ecg record (without extension)
     :param patient_id:
     :param metadata:
     :return: ECGRecord
     """
-    rec = wfdb.rdrecord(rec_path)
+    base_path = Path(rec_path) / Path(rec_name)
+    dat_file = Path(f"{base_path}.dat")
+    hea_file = Path(f"{base_path}.hea")
+
+    if not hea_file.is_file():
+        raise FileNotFoundError(f".hea file not found: {hea_file}")
+
+    if not dat_file.is_file():
+        raise FileNotFoundError(f".dat file not found: {dat_file}")
+
+    rec = wfdb.rdrecord(base_path)
     leads = ECGLeads(
         **dict(zip(rec.sig_name, rec.p_signal.transpose()))
     )
